62214cb98eb21e2158d9f7cf525caafcd1b7887c
Artyom Harutyunyan <ka6rias@gmail.com>
2023-09-01 21:15:09 +0400

    factorial task finished

---
+++ Recursion/factorial.js
@@ -0,0 +1,8 @@
+//Write a recursive function to calculate the factorial of a given positive integer
+
+function factorial(n) {
+    if (n < 0)
+        return 'number should be positive'
+    return n === 0 || n === 1 ? 1 : n * factorial(n - 1)
+}
+console.log(factorial(3));
