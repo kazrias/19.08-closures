3b1d5ccff23b0b9c21ffb289140c9e8470f2a008
Artyom Harutyunyan <ka6rias@gmail.com>
2023-09-01 23:27:20 +0400

    palindrome task finished

--- Recursion/palidrome-05.js
+++ Recursion/palidrome-05.js
@@ -1,1 +1,9 @@
 //Write a recursive function to check if a given string is a palindrome
+
+function isPalindrome(word) {
+  if (word.length < 2)
+    return true
+  if (word[0] === word.slice(-1))
+    return isPalindrome(word(1, -1))
+  return false
+}
