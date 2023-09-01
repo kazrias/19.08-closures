f979b8bb0a12b7fd829274698a88449a90d6802e
Artyom Harutyunyan <ka6rias@gmail.com>
2023-09-01 21:41:42 +0400

    exponent task finished

--- Recursion/exponent.js
+++ Recursion/exponent.js
@@ -1,1 +1,11 @@
 //Write a recursive function to calculate the value of a number raised to an exponent
+function exponent(val, pow) {
+    if (pow !== 0) {
+        if (pow > 0)
+            return val * exponent(val, pow - 1);
+        else
+            return 1 / exponent(val, -pow);
+    } else {
+        return 1;
+    }
+}   
