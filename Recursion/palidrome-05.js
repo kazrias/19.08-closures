64bc0764d4e6441b017000d63169c3cfb69c9f3f
Artyom Harutyunyan <ka6rias@gmail.com>
2023-09-01 23:21:43 +0400

    changed

---
+++ Recursion/palidrome-05.js
@@ -0,0 +1,1 @@
+//Write a recursive function to check if a given string is a palindrome
--- Recursion/sum-04.js
+++ Recursion/sum-04.js
@@ -1,4 +1,5 @@
 // Write a recursive function to calculate the sum of an array of numbers
+
 function sum(arr) {
   return !arr.length ? 0 : arr[0] + sum(arr.slice(1))
 }
