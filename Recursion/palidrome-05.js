df0ad90961c3d4e60b9ff3e8e3703e7393107808
Artyom Harutyunyan <ka6rias@gmail.com>
2023-09-07 20:28:09 +0400

    isPalidrome function fixed

--- Recursion/palidrome-05.js
+++ Recursion/palidrome-05.js
@@ -4,6 +4,6 @@ function isPalindrome(word) {
   if (word.length < 2)
     return true
   if (word[0] === word.slice(-1))
-    return isPalindrome(word(1, -1))
+    return isPalindrome(word.slice(1, -1))
   return false
 }
